14787ecd8a02aa9760557c79aeeed067f970ca94
Kanish garg <kanish1178.becse24@chitkara.edu.in>
2025-03-29 00:17:48 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+
 int isSorted(int age[], int n) {
-    for (int i = 1; i < n - 1; i++) {
-        if (age[i+1] > age[i]||age[i]<age[i-1]) {
-            return i;
+    for (int i = 1; i < n; i++) {  // Corrected loop condition
+        if (age[i] < age[i - 1]) {  // Check for unsorted condition
+            return i;  // Return the first index where unsorted
         }
     }
-    }
-int main(){
-    int a,n,i;
-    int age[a];
-    scanf("%d",&n );
-    for(int c=0;c<n-1;c++){
-        scanf("%d",&age[i]);
-    }
-    if (isSorted(age,n)){
-        printf("%d",i);
+    return -1;  // Return -1 if sorted
+}
+
+int main() {
+    int n, i;
+    scanf("%d", &n);  // Input number of elements
+
+    int age[n];  // Declare array with size n
+
+    for (i = 0; i < n; i++) {  // Corrected the loop condition and indexing
+        scanf("%d", &age[i]);  // Input the array elements
     }
 
-}    
+    int res = isSorted(age, n);
+
+    if (res == -1) 
+    else {
+        printf("%d\n", res);
+    }
+
+    return 0;  // Return success
+}
+
